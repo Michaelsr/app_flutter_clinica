c2da75b43117c6498adbfca1479442b13fbfbc61
Michaelsr <michaelsonccoramos@gmail.com>
2022-12-13 18:20:18 -0500

    commit theme

--- lib/main.dart
+++ lib/main.dart
@@ -19,7 +19,7 @@ class MyApp extends StatelessWidget {
         'login': (BuildContext context) => const LoginPage(),
       },
       theme: ThemeData(
-        primarySwatch: Colors.blue,
+        primarySwatch: Colors.yellow,
       ),
     );
   }
